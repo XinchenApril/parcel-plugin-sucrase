1e8c553b79f71c2521b6fa67349b4ab38e7fce02
Xinchen Huang <40370952+XinchenApril@users.noreply.github.com>
2020-04-13 18:21:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = function(bundler){
-    bundler.addAssetType('.js', require.resolve('./assets/SucraseAsset'))
+    bundler.addAssetType('.js', require.resolve('./assets/SucraseAsset'));
+    bundler.addAssetType('.ts', require.resolve('./assets/SucraseAsset'))
+    bundler.addAssetType('.tsx', require.resolve('./assets/SucraseAsset'))
+    bundler.addAssetType('.jsx', require.resolve('./assets/SucraseAsset'))
 }
 
